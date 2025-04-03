f2b6e868abbd2a952bdbb9841924ec768496bace
masrur447 <m.masrur.official@gmail.com>
2025-04-03 19:40:34 +0600

    configure Tailwind CSS and add primary color theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,22 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  app: {
+    head: {
+      bodyAttrs: {
+        class: "bg-primary text-white antialiased",
+      },
+    },
+  },
   modules: ["@nuxtjs/tailwindcss", "@nuxt/icon"],
   tailwindcss: {
     cssPath: "~/assets/css/tailwind.css",
     exposeConfig: true,
     viewer: true,
-    config: {
-      content: [
-        "./app.vue",
-        "./components/**/*.{vue,js,ts,jsx,tsx}",
-        "./layouts/**/*.{vue,js,ts,jsx,tsx}",
-        "./pages/**/*.{vue,js,ts,jsx,tsx}",
-        "./plugins/**/*.{js,ts}",
-        "./app/**/*.{js,ts}",
-        "./content/**/*.md",
-        "./content/**/*.svx",
-      ],
-    },
   },
 });
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app.vue",
+    "./components/**/*.{vue,js,ts,jsx,tsx}",
+    "./layouts/**/*.{vue,js,ts,jsx,tsx}",
+    "./pages/**/*.{vue,js,ts,jsx,tsx}",
+    "./plugins/**/*.{js,ts}",
+    "./app/**/*.{js,ts}",
+    "./content/**/*.md",
+    "./content/**/*.svx",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#000319",
+      },
+    },
+  },
+  plugins: [],
+};

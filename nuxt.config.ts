c484328b2a1d9d4ee434d686cba3989e848aa468
masrur447 <m.masrur.official@gmail.com>
2025-04-03 19:09:48 +0600

    add initial Nuxt.js pages and Tailwind CSS configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon"],
+  tailwindcss: {
+    cssPath: "~/assets/css/tailwind.css",
+    exposeConfig: true,
+    viewer: true,
+    config: {
+      content: [
+        "./app.vue",
+        "./components/**/*.{vue,js,ts,jsx,tsx}",
+        "./layouts/**/*.{vue,js,ts,jsx,tsx}",
+        "./pages/**/*.{vue,js,ts,jsx,tsx}",
+        "./plugins/**/*.{js,ts}",
+        "./app/**/*.{js,ts}",
+        "./content/**/*.md",
+        "./content/**/*.svx",
+      ],
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+    <NuxtWelcome />
+</template>

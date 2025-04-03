5b227c4a5c991e124965db47a0419b95a27a5fec
masrur447 <m.masrur.official@gmail.com>
2025-04-03 20:53:55 +0600

    update Hero component styles for improved responsiveness and readability

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,14 +1,17 @@
 <template>
-    <section class="flex flex-col items-center justify-center bg-primary text-white py-32">
-        <p class="text-center text-[#E4ECFF] font-sans font-medium text-base/[120%] uppercase tracking-[4px] py-4">
+    <section class="flex flex-col items-center justify-center bg-primary text-white py-32 px-5">
+        <p
+            class="text-center text-[#E4ECFF] font-sans font-medium text-sm md:text-base/[120%] uppercase tracking-[4px] py-4">
             Dynamic
             Web Magic with
             Next.js</p>
-        <h1 class="font-sans font-bold text-7xl text-center">Transforming Concepts into
+        <h1 class="font-sans font-bold text-3xl/[120%] md:text-7xl text-center">Transforming Concepts into
             <br />
             Seamless <span class="text-[#CBACF9]">User Experiences</span>
         </h1>
-        <p class="text-center font-sans text-2xl/[120%] font-normal capitalize py-5">Hi! I’m Eyaheya Masrur, a Next.js
+        <p class="text-center font-sans md:text-2xl/[120%] font-normal capitalize py-5">Hi! I’m Eyaheya Masrur,
+            a
+            Next.js
             Developer
             based in
             Croatia.</p>

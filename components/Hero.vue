31718cec341ca2273d3d125cc14e846cd33173f1
masrur447 <m.masrur.official@gmail.com>
2025-04-04 10:52:25 +0600

    uncommited

--- components/Hero.vue
+++ components/Hero.vue
@@ -2,23 +2,20 @@
     <section class="flex flex-col items-center justify-center bg-primary text-white py-32 px-5">
         <p
             class="text-center text-[#E4ECFF] font-sans font-medium text-sm md:text-base/[120%] uppercase tracking-[4px] py-4">
-            Dynamic
-            Web Magic with
-            Next.js</p>
-        <h1 class="font-sans font-bold text-3xl/[120%] md:text-7xl text-center">Transforming Concepts into
+            Dynamic Web Magic with Next.js
+        </p>
+        <h1 class="font-sans font-bold text-3xl/[120%] md:text-7xl text-center">
+            Transforming Concepts into
             <br />
             Seamless <span class="text-[#CBACF9]">User Experiences</span>
         </h1>
-        <p class="text-center font-sans md:text-2xl/[120%] font-normal capitalize py-5">Hi! I’m Eyaheya Masrur,
-            a
-            Next.js
-            Developer
-            based in
-            Croatia.</p>
+        <p class="text-center font-sans md:text-2xl/[120%] font-normal capitalize py-5">
+            Hi! I’m Eyaheya Masrur, a Next.js Developer based in Croatia.
+        </p>
         <button
             class="border border-[#CBACF9] bg-[#CBACF9]/[10%] text-white font-sans font-bold text-base/[120%] py-4 px-8 rounded-md hover:bg-transparent hover:text-[#CBACF9] transition duration-300 ease-in-out">
             See My Work
-            <Icon name="ph:arrow-up-bold rotate-45" class="inline-block" />
+            <Icon name="ph:arrow-up-bold" class="inline-block rotate-45" />
         </button>
     </section>
 </template>

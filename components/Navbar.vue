b327308538e3d99e65d45e4b68d729147c032a20
masrur447 <m.masrur.official@gmail.com>
2025-04-05 21:18:03 +0600

    feat: enhance Navbar with scroll effect and styling adjustments

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -25,15 +25,41 @@ const menus: Menu[] = [
     }
 ]
 
+watchEffect(() => {
+    if (typeof window === 'undefined') return;
+    const navbar = document.querySelector('.scroll-navbar') as HTMLElement;
+    const scrollHandler = () => {
+        if (window.scrollY > 0) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    };
+    window.addEventListener('scroll', scrollHandler);
+    return () => {
+        window.removeEventListener('scroll', scrollHandler);
+    };
+});
+
 </script>
 
 <template>
     <nav
-        class="fixed top-5 bg-gradient-to-r from-[#0C0E23] to-[#04071D] text-white py-4 px-6 rounded-md inline-block border border-[#1D2029] z-50">
-        <ul class="flex justify-center space-x-8 py-4">
+        class="scroll-navbar w-full fixed top-0 bg-gradient-to-r from-[#0C0E23] to-[#04071D] text-white py-4 px-4 md:px-6 rounded-md inline-block border border-[#1D2029] z-50">
+        <ul class="flex justify-center space-x-4 md:space-x-8 py-4">
             <li v-for="(menu, index) in menus" :key="index" class="font-sans font-medium text-base">
                 <LazyNuxtLink :href="menu.href">{{ menu.title }}</LazyNuxtLink>
             </li>
         </ul>
     </nav>
 </template>
+
+<style scoped>
+.scroll-navbar {
+    transition: all 0.3s ease-in-out;
+}
+
+.scroll-navbar.scrolled {
+    background: rgba(255, 255, 255, 0.1) !important;
+}
+</style>

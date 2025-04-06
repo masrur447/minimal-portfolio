d8630a4f83b3a0ecf0dfb9aac2a056c08e52a634
masrur447 <m.masrur.official@gmail.com>
2025-04-06 10:36:35 +0600

    feat: update ProjectCard layout for improved spacing and alignment

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -29,7 +29,8 @@ defineProps({
             <p class="text-sm md:text-xl line-clamp-2 py-2">
                 {{ project.description }}
             </p>
-            <div class="flex flex-col items-center justify-center md:justify-between space-y-2 md:space-y-0 py-2">
+            <div
+                class="flex flex-col items-center justify-center gap-4 md:flex-row md:justify-between  space-y-2 md:space-y-0 py-2">
                 <div class="flex">
                     <button class="flex items-center justify-center w-10 h-10 rounded-full bg-[#13162D] mr-2">
                         <img src="/assets/icons/react.svg" class="size-full object-cover" alt="" />
